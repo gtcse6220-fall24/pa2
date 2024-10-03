6485232c3f2e06404ab29e10118857bd4a3c08aa
Abhishek Kalokhe <abhisheknkalokhe@gmail.com>
2024-10-02 23:37:59 -0400

    Update autograder

--- autograder.sh
+++ autograder.sh
@@ -4,11 +4,11 @@
 #SBATCH --output=mpi_results.out
 #SBATCH --error=mpi_results.err
 #SBATCH --nodes=1
-#SBATCH --time=00:30:00
-#SBATCH --exclusive
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=00:05:30
 
 # Maximum time allowed for the script (in seconds)
-MAX_EXECUTION_TIME=1800
+MAX_EXECUTION_TIME=300
 
 # Run the tests within the time limit
 timeout $MAX_EXECUTION_TIME bash -c '
@@ -27,23 +27,56 @@ echo " "
 echo "=================== CORRECTNESS TESTS ==================="
 echo " "
 echo "##Scatter"
-srun -n 16 ./primitives -s 100000000 5
-srun -n 24 ./primitives -s 100000000 5
+output_16=$(srun -n 16 ./primitives -s 100000000 5)
+output_24=$(srun -n 24 ./primitives -s 150000000 5)
+echo "$output_16"
+echo "----------------------------------"
+echo "$output_24"
+if [[ $output_16 != *"Implementation is correct!"* ]] || [[ $output_24 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
+fi
+echo "=================================="
 echo " "
 echo "##AllGather"
-srun -n 16 ./primitives -g 100000000 0
-srun -n 24 ./primitives -g 100000000 0
+output_16=$(srun -n 16 ./primitives -g 100000000 0)
+output_24=$(srun -n 24 ./primitives -g 150000000 0)
+echo "$output_16"
+echo "----------------------------------"
+echo "$output_24"
+if [[ $output_16 != *"Implementation is correct!"* ]] || [[ $output_24 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
+fi
+echo "=================================="
 echo " "
 echo "##AllReduce"
-srun -n 16 ./primitives -r 100000000 0
-srun -n 24 ./primitives -r 100000000 0
+output_16=$(srun -n 16 ./primitives -r 100000000 0)
+output_24=$(srun -n 24 ./primitives -r 150000000 0)
+echo "$output_16"
+echo "----------------------------------"
+echo "$output_24"
+if [[ $output_16 != *"Implementation is correct!"* ]] || [[ $output_24 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
+fi
+echo "=================================="
 echo " "
 echo "##AlltoAll_Arbitrary"
-srun -n 16 ./primitives -a 100000000 0
-srun -n 24 ./primitives -a 100000000 0
+output_16=$(srun -n 16 ./primitives -a 100000000 0)
+output_24=$(srun -n 24 ./primitives -a 150000000 0)
+echo "$output_16"
+echo "----------------------------------"
+echo "$output_24"
+if [[ $output_16 != *"Implementation is correct!"* ]] || [[ $output_24 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
+fi
+echo "=================================="
 echo " "
 echo "##AlltoAll_Hypercube"
-srun -n 16 ./primitives -h 100000000 0
+output_16=$(srun -n 16 ./primitives -h 100000000 0)
+echo "$output_16"
+if [[ $output_16 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
+fi
+echo "=================================="
 echo " "
 
 
@@ -54,71 +87,94 @@ multiplier=25
 echo "##Scatter"
 output_16=$(srun -n 16 ./primitives -s 100000000 0)
 echo "$output_16"
-custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
-mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
-
-if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
-    echo "Runtime test passed!"
+if [[ $output_16 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
 else
+    custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
+    mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
+
+    if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
+        echo "Runtime test passed!"
+    else
     echo "Runtime test failed"
+    fi
 fi
 
+echo "=================================="
 echo " "
 echo "##AllGather"
 multiplier=3
 output_16=$(srun -n 16 ./primitives -g 100000000 0)
 echo "$output_16"
-custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
-mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
-
-if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
-    echo "Runtime test passed!"
+if [[ $output_16 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
 else
-    echo "Runtime test failed"
+	custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
+	mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
+
+	if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
+    		echo "Runtime test passed!"
+	else
+    	echo "Runtime test failed"
+	fi
 fi
 
+echo "=================================="
 echo " "
 echo "##AllReduce"
 multiplier=10
 output_16=$(srun -n 16 ./primitives -r 100000000 0)
 echo "$output_16"
-custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
-mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
-
-if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
-    echo "Runtime test passed!"
+if [[ $output_16 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
 else
-    echo "Runtime test failed"
+	custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
+	mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
+
+	if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
+    		echo "Runtime test passed!"
+	else
+    		echo "Runtime test failed"
+	fi
 fi
 
+echo "=================================="
 echo " "
 echo "##AlltoAll_Arbitrary"
 multiplier=2
 output_16=$(srun -n 16 ./primitives -a 100000000 0)
 echo "$output_16"
-custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
-mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
-
-if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
-    echo "Runtime test passed!"
+if [[ $output_16 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
 else
-    echo "Runtime test failed"
+	custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
+	mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
+
+	if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
+    		echo "Runtime test passed!"
+	else
+    		echo "Runtime test failed"
+	fi
 fi
 
+echo "=================================="
 echo " "
 echo "##AlltoAll_Hypercube"
 multiplier=10
 output_16=$(srun -n 16 ./primitives -h 100000000 0)
 echo "$output_16"
-custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
-mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
-
-if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
-    echo "Runtime test passed!"
+if [[ $output_16 != *"Implementation is correct!"* ]]; then
+    echo "ERROR"
 else
-    echo "Runtime test failed"
-fi
+	custom_16=$(echo "$output_16" | grep "Custom_" | awk "{print \$3}")
+	mpi_16=$(echo "$output_16" | grep "MPI_" | awk "{print \$3}")
 
+	if (( $(echo "$custom_16 < $multiplier * $mpi_16" | bc -l) )); then
+    		echo "Runtime test passed!"
+	else
+    		echo "Runtime test failed"
+	fi
+fi
 
 echo " "
 
